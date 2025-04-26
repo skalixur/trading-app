3b16b92d84e164f020ab009c9e6ca25e18f0c15e
skalixur <juanmateod04@gmail.com>
2025-04-26 11:26:55 +0800

    user model: add validations and helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, presence: true
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
+  def admin?
+    is_admin
+  end
+
+  def approved?
+    is_approved
+  end
+
+  def activate_for_authentication?
+    super && approved?
+  end
 end
