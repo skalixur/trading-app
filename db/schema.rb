2f822243306c59f2b67bcc677d2e0980cae2c93b
JennieLin08 <jennelyn.hernandez2578@gmail.com>
2025-05-04 15:25:44 +0800

    add unconfirmed_email colum for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_30_123721) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_04_072135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_30_123721) do
     t.decimal "balance"
     t.boolean "is_approved", default: false
     t.boolean "is_admin", default: false
+    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
