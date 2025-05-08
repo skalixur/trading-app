5b081396ba8fa61471af9ff5afe3f7d4ac595f8a
JennieLin08 <jennelyn.hernandez2578@gmail.com>
2025-05-08 20:16:21 +0800

    install/setup rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'does not allow creation of user with invalid attribute values' do
+      user = User.new(name:"jen",email: "mail@mail.com",password: nil)
+      expect(user).not_to be_valid
+    end
+  end
+end
