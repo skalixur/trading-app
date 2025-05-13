881ae2eecb2ff62ed0b61a4469f5a2cf55ad7a9b
JennieLin08 <jennelyn.hernandez2578@gmail.com>
2025-05-13 19:17:41 +0800

    add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,52 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  context 'validations' do
-    it 'does not allow creation of user with invalid attribute values' do
-      user = User.new(name:"jen",email: "mail@mail.com",password: nil)
-      expect(user).not_to be_valid
+   let(:valid_attributes) do
+    {
+      name: "test",
+      email: "test@mail.com",
+      password: "password",
+      password_confirmation: "password",
+      is_approved: false, 
+      is_admin: false,
+      confirmed_at: Time.current 
+    }
+  end
+
+  it "is valid with valid attributes" do
+    user = User.new(valid_attributes)
+    expect(user).to be_valid
+  end
+
+  it "is not valid without an email" do
+    user = User.new(valid_attributes.merge(email: nil))
+    expect(user).not_to be_valid
+  end
+
+  it "is not valid with mismatched password confirmation" do
+    user = User.new(valid_attributes.merge(password_confirmation: "wrongpass"))
+    expect(user).not_to be_valid
+  end
+
+  it "is not valid without a password" do
+    user = User.new(valid_attributes.merge(password: nil))
+    expect(user).not_to be_valid
+  end
+
+  it "is not confirmed by default" do
+    user = User.new(valid_attributes.except(:confirmed_at))
+    expect(user.confirmed?).to be_falsey
+  end
+
+  context "when saving a duplicate email" do
+    before do
+      User.create!(valid_attributes)
+    end
+
+    it "is not valid with the same email" do
+      duplicate_user = User.new(valid_attributes)
+      expect(duplicate_user).not_to be_valid
     end
   end
+
 end
